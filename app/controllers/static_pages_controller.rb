e1333c7c644478fbe7bda80b1a7f41fd1467d6fc
Shiva <shiva.n404@gmail.com>
2012-10-03 23:38:23 +0530

    -Initial commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+    @micropost = current_user.microposts.build if signed_in?
+    @feed_items = current_user.feed.paginate(:page => params[:page]) if signed_in?
+  #  @feed_items = current_user.feed.all
+  end
+
+  def help
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaticTest::Application.config.secret_token = '3ba60eeb0c0058825e44ed6c1d2aaf8906f9f5f83f16d1a49d9f6d0801d43ca9ad8cd632786b6ed1bdfce9af691c18c5152f04a0fa0d25f6dfa1cab75563ceba'
